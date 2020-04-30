4e6c228defc2855c83dd9f9aae02699c4a2cc306
vima892 <vima892@gmail.com>
2020-04-30 13:56:10 +0200

    php-validation-email-test-1

--- process.php
+++ process.php
@@ -1,7 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $u_email = $_POST["user_email"];
-    echo "<p>" . $u_email . "</p>";
+	$u_email = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
+	if (empty($u_email) || !filter_var($u_email, FILTER_VALIDATE_EMAIL)) {
+		die("Please enter a valid email address!");
+	}
+    echo "<p>" . $u_email . "</p>"; //DEBUG STATEMENT
 	//mysql credentials
 	//$mysql_host = "localhost";
 	//$mysql_username = "site"<
