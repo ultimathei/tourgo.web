db9d121242e8bfae398510cec02268179ea423f7
vima892 <vima892@gmail.com>
2020-04-30 13:31:26 +0200

    php-test

---
+++ process.php
@@ -0,0 +1,11 @@
+<?php
+if ($_SERVER["REQUEST METHOD"] == "POST") {
+    $u_email = $_POST["user_email"];
+    print $u_email;
+	//mysql credentials
+	//$mysql_host = "localhost";
+	//$mysql_username = "site"<
+	//$mysql_password = "`xE3pp<Pq7u&L@U-";
+	//$mysql_database = "tourgoapp";
+}
+?>
