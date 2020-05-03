4874e82d9d3f73fe02f4f59cd51c50f4bbc71a3b
ultimathei <krisztian.mate.design@gmail.com>
2020-05-03 01:17:02 +0100

    testing video animation (json)

---
+++ app/vid.js
@@ -0,0 +1,15 @@
+/* Play an animation on each click */
+let iconSkipForward = document.getElementById('bm');
+
+let animationSkipForward = bodymovin.loadAnimation({
+        container: iconSkipForward,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: '/video/data.json'
+});
+
+  iconSkipForward.addEventListener('click', function() {
+  animationSkipForward.play();
+  animationSkipForward.addEventListener('complete', ()=>animationSkipForward.goToAndStop(0, true));
+});
