c4ca1575f97596cc3aac499b75d563835f6ed425
ultimathei <krisztian.mate.design@gmail.com>
2020-05-03 02:07:34 +0100

    ..

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
   //initially display play button
   // -> only trigger fetch on click
   // -> display loader while fetching JSON data
-  $.getJSON('/video/data.json', (data)=>renderIntro(data));
+  $.getJSON('../video/data.json', (data)=>renderIntro(data));
 
   /*
   //TODO
