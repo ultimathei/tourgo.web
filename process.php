d9c0fade3eac4b646d21bb3efb6db39f9cbd653b
vima892 <vima892@gmail.com>
2020-05-14 18:19:16 +0200

    Version zero email check fix

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$u_email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
 	if (empty($u_email) || !filter_var($u_email, FILTER_VALIDATE_EMAIL)){
-		die("Please enter a valid email address!");
+		echo "<p>Please enter a valid email address!</p>";
 		sleep(2);
 		header("location: ./index.php");
 	}
