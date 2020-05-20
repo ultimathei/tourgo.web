6d00efc13870e47fd5acd7ea34aef78d53c6a18a
vima892 <vima892@gmail.com>
2020-05-20 15:48:30 +0200

    Compacted all in one file

--- process.php
+++ process.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	if(isset($_POST['likeToCompete'])){
 		if ($result = mysqli_query($con, "INSERT INTO emails VALUES ('$u_email', true)")){
 			mysqli_free_result($result);
-			header("location: mailer.php")
+			mailer($u_email);
 			//header("location: https://docs.google.com/forms/d/e/1FAIpQLSdsjUgnHmWLVST8QgSkfMc_OaMQIzpx9xhM2J1pgoLh58amyQ/viewform");
 		}
 	} else {
@@ -42,5 +42,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 		}
 	}
 	mysqli_close($con);
+}
+
+function mailer($user_email) {
+	echo "pippo";
+	// Instantiation and passing `true` enables exceptions
+	$mail = new PHPMailer;
+
+	//Server settings
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+	$mail->isSMTP();                                            // Send using SMTP
+	$mail->Host       = 'mail.tourgoapp.com';                    // Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+	$mail->Username   = 'company@tourgoapp.com';                     // SMTP username
+	$mail->Password   = '4gcBjf5cTP';                               // SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+	$mail->Port       = 995;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+	//Recipients
+	$mail->setFrom('company@tourgoapp.com', 'TOURGO LTD');
+	$mail->addAddress($user_email, 'Joe User');     // Add a recipient
+	
+
+	// Attachments
+	$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+	$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+	// Content
+	$mail->isHTML(true);                                  // Set email format to HTML
+	$mail->Subject = 'Here is the subject';
+	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+	if (!$mail->send()) {
+		$msg .= 'Mailer Error: '. $mail->ErrorInfo;
+	} else {
+		$msg .= 'Message sent!';
+	}
 }
 ?>
