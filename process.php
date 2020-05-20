ad7940d9154e5909f7a4f3e7749783e4698d0252
vima892 <vima892@gmail.com>
2020-05-20 14:35:07 +0200

    PHP mailer test 1

--- process.php
+++ process.php
@@ -1,4 +1,13 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$u_email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
 	if (empty($u_email) || !filter_var($u_email, FILTER_VALIDATE_EMAIL)){;
@@ -29,6 +38,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	} else {
 		if ($result = mysqli_query($con, "INSERT INTO emails VALUES ('$u_email', false)")){
 			mysqli_free_result($result);
+			// Instantiation and passing `true` enables exceptions
+			$mail = new PHPMailer(true);
+
+			try {
+				//Server settings
+				$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+				$mail->isSMTP();                                            // Send using SMTP
+				$mail->Host       = 'localhost';                    // Set the SMTP server to send through
+				$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+				$mail->Username   = 'company@tourgoapp.com';                     // SMTP username
+				$mail->Password   = '4gcBjf5cTP';                               // SMTP password
+				$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+				$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+				//Recipients
+				$mail->setFrom('company@tourgoapp.com', 'TOURGOLTD');
+				$mail->addAddress($u_email);     // Add a recipient
+				//$mail->addReplyTo('info@example.com', 'Information');
+				//$mail->addCC('cc@example.com');
+				//$mail->addBCC('bcc@example.com');
+
+				// Attachments
+				//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+				//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+				// Content
+				$mail->isHTML(true);                                  // Set email format to HTML
+				$mail->Subject = 'Test mail';
+				$mail->Body    = '<h1>WELCOME TO TOURGO!!!!!</h1>';
+				$mail->AltBody = 'Boi u suck';
+
+				$mail->send();
+				echo 'Message has been sent';
+			} catch (Exception $e) {
+				echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+			}
 			header("location: https://docs.google.com/forms/d/e/1FAIpQLSdsjUgnHmWLVST8QgSkfMc_OaMQIzpx9xhM2J1pgoLh58amyQ/viewform");
 
 		}
